163c68ec04f4121ea21fd87ab446e0810e4812bf
Bhanu prakash <sigebhanuprakash171@gmail.com>
2025-02-10 14:39:35 +0530

    cors

--- index.js
+++ index.js
@@ -20,7 +20,12 @@ const __dirname = dirname(__filename);
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+  origin: "*", 
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+}));
+
 app.use(cookieParser());
 
 const storage = multer.diskStorage({
